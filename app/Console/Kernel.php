2a85a21f0f18a83ed8ad99981776a8758d5f94d1
ekowebdev <ekoprasetyo.webdev@gmail.com>
2023-10-01 21:45:42 +0700

    Update template email birth day wish

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:birthdaywish')->dailyAt('09:00');
+        $schedule->command('send:birthdaywish')->dailyAt('08:00');
     }
 
     /**
--- resources/views/emails/birthday_wish.blade.php
+++ resources/views/emails/birthday_wish.blade.php
@@ -4,6 +4,9 @@
     <title>Birth Day Wish</title>
 </head>
 <body>
-    <h1 style="color: #333;">Happy birthday {{ $user->name }}!!</h1>
+    <h2>Hey, Happy Birthday {{ $user->name }}</h2>
+    <br>
+    <p>On behalf of <strong>Baktiweb</strong> I wish you a very happy birthday and send you my best wishes for much happiness in your life.</p>
+    Thank you
 </body>
 </html>
