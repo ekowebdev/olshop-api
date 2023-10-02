b2ab5274c1f4fcb437be08af5f576ca98e4a13dd
ekowebdev <ekoprasetyo.webdev@gmail.com>
2023-10-02 11:18:35 +0700

    Updated

--- app/Console/Commands/SendBirthDayWish.php
+++ app/Console/Commands/SendBirthDayWish.php
@@ -6,6 +6,7 @@ use App\Http\Models\User;
 use App\Mail\BirthDayWish;
 use Illuminate\Console\Command;
 use App\Jobs\SendEmailBirtDayWishJob;
+use Illuminate\Support\Facades\Cache;
 
 class SendBirthDayWish extends Command
 {
@@ -30,12 +31,25 @@ class SendBirthDayWish extends Command
      */
     public function handle()
     {
-        $users = User::whereRaw('MONTH(birthdate) = ? AND DAY(birthdate) = ?', [now()->month, now()->day])->get();
+        // $users = User::whereRaw('MONTH(birthdate) = ? AND DAY(birthdate) = ?', [now()->month, now()->day])->get();
 
-        foreach ($users as $user) {
+        // foreach ($users as $user) {
+        //     if (!Cache::has('birthday_notification_sent_' . $user->id)) {
+        //         SendEmailBirtDayWishJob::dispatch($user);
+        //         Cache::put('birthday_notification_sent_' . $user->id, true, now()->addDay());
+        //     }
+        // }
+
+        // $this->info('Birthday wish notifications sent successfully.');
+
+        $i = 0;
+        $users = User::whereMonth('birthdate', '=', date('m'))->whereDay('birthdate', '=', date('d'))->get();  
+
+        foreach($users as $user) {
             SendEmailBirtDayWishJob::dispatch($user);
+            $i++;
         }
 
-        $this->info('Birthday wish notifications sent successfully.');
+        $this->info($i.' Birthday wish messages sent successfully!');
     }
 }
