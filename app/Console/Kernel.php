5a0139aabb787ef483c19ae2c31855acff8d827c
ekowebdev <ekoprasetyo.webdev@gmail.com>
2023-10-06 10:10:31 +0700

    Update scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:birthdaywish')->dailyAt('10:10');
+        $schedule->command('send:birthdaywish')->dailyAt('10:13');
     }
 
     /**
