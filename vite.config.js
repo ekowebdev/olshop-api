40ef54c966ad68c797b27ed6636fb8a9b3b8eb3c
ekowebdev <ekoprasetyo.webdev@gmail.com>
2023-10-22 15:14:44 +0700

    Updated ci/cd

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        // laravel({
-        //     input: ['resources/css/app.css', 'resources/js/app.js'],
-        //     refresh: true,
-        // }),
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
