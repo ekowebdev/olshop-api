15a7634b8d0ad2ff5d319f8ea77f448b31386d31
eko10 <eko3227@gmail.com>
2023-11-05 08:41:58 +0700

    Fix error vite

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/sass/app.scss',
-                'resources/js/app.js',
             ],
             refresh: true,
         }),
