ad5c84818836879b59e59e06b46d876b44a807a6
eko10 <eko3227@gmail.com>
2023-12-02 23:44:18 +0700

    Update payment log resource

--- app/Http/Resources/PaymentLogResource.php
+++ app/Http/Resources/PaymentLogResource.php
@@ -20,6 +20,7 @@ class PaymentLogResource extends JsonResource
                 'redeem_date' => Carbon::parse($this->redeems->created_at)->format('Y-m-d H:i:s'),
                 'fredeem_date' => Carbon::parse($this->redeems->created_at)->diffForHumans(),
                 'note' => $this->redeems->note,
+                'snap_token' => $this->redeems->snap_token,
                 'snap_url' => $this->redeems->snap_url,
                 'metadata' => json_decode($this->redeems->metadata),
                 'redeem_status' => $this->redeems->redeem_status,
